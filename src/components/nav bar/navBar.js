95622f06fc042767955bf05a2c3464da9c2f38bd
Eduardo Aguilar <eaguilar92105@yahoo.com>
2020-08-21 19:34:09 -0700

    imported router library and working on nav bar

--- src/components/nav bar/navBar.js
+++ src/components/nav bar/navBar.js
@@ -1,1 +1,32 @@
 import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Eduardo Aguilar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="Home.html">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                navigation
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="Portfolio.html">Portfolio</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="Contact.html">Contact</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+};
+
+export default Nav;
